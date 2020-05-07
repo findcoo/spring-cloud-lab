82e54a991c4bae7f906d7a7cd91a7d03ab55385d
coo.find <coo.find@coofindui-MacBookPro.local>
2020-05-07 18:06:59 +0900

    Fix simple-task

--- task/simple-task/src/main/kotlin/com/github/findcoo/task/JobConfig.kt
+++ task/simple-task/src/main/kotlin/com/github/findcoo/task/JobConfig.kt
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.launch.support.RunIdIncrementer
 import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.context.annotation.Bean
@@ -13,42 +14,26 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class JobConfig(private val jobBuilderFactory: JobBuilderFactory, private val stepBuilderFactory: StepBuilderFactory) {
-    companion object {
-        private val log: Logger = LoggerFactory.getLogger(JobConfig::class.java)
-    }
+  companion object {
+    private val log: Logger = LoggerFactory.getLogger(JobConfig::class.java)
+  }
 
-    @Bean
-    fun helloTask(): Tasklet {
-        return Tasklet { _, _ ->
-            log.info("hello")
-            RepeatStatus.FINISHED
-        }
+  @Bean
+  fun helloTask(): Tasklet {
+    return Tasklet { _, _ ->
+      log.info("hello")
+      RepeatStatus.FINISHED
     }
+  }
 
-    @Bean
-    fun worldTask(): Tasklet {
-        return Tasklet { _, _ ->
-            log.info("world")
-            RepeatStatus.FINISHED
-        }
-    }
-
-    @Bean
-    fun job1(helloTask: Tasklet): Job {
-        return jobBuilderFactory.get("job1")
-                .start(stepBuilderFactory.get("step1")
-                        .tasklet(helloTask)
-                        .build())
-                .build()
-    }
-
-    @Bean
-    fun job2(worldTask: Tasklet): Job {
-        return jobBuilderFactory.get("job1")
-                .start(stepBuilderFactory.get("step1")
-                        .tasklet(worldTask)
-                        .build())
-                .build()
-    }
+  @Bean
+  fun job2(helloTask: Tasklet): Job {
+    return jobBuilderFactory.get("job1")
+      .incrementer(RunIdIncrementer())
+      .start(stepBuilderFactory.get("step1")
+        .tasklet(helloTask)
+        .build())
+      .build()
+  }
 
 }
